5499bd783c4bba41e33972ddf78a729bd496bda0
Muhammad Hussain <muhammad.a.hussain@outlook.com>
2019-10-30 10:17:09 +0000

    create ship class

---
+++ src/ship.js
@@ -0,0 +1,25 @@
+class ship {
+
+    constructor(name) {
+        this.name = name;
+        this.width = 1;
+        this.height = 4;
+        this.health = 1;
+        this.isSunk = false;
+    }
+
+    getHealth() {
+        return this.health;
+    }
+
+    setHealth(hitDamage) {
+        this.health - hitDamage;
+    }
+
+    setIsAlive(status) {
+        this.status = status;
+    }
+
+}
+
+module.exports.ship;
