d1f3cf0094a8cf3a19c3d5185e8442b3b57b95d9
Muhammad Hussain <muhammad.a.hussain@outlook.com>
2020-01-09 18:53:23 +0000

    feat: add ability to place ships on a board

--- simple-battle-ships/src/board.js
+++ simple-battle-ships/src/board.js
@@ -14,9 +14,31 @@ class board {
     }
   }
 
-  getBoard() {
-    return grid;
-  }
+	getBoard() {
+		return board;
+	}
+
+	placeShip(ship, coordinates, orientation = 'vertical') {
+		const shipLength = ship.getLength();
+
+		if(orientation === 'vertical') {
+			for (let x = 0; x < shipLength; x++) {
+				if (ship.getCoordinates().length === 0) {
+					board[coordinates.y - 1][coordinates.x - 1] = '\u2227';
+					ship.setCoordinates(coordinates, x);
+				} else if (x === shipLength - 1) {
+					board[coordinates.y - 1][coordinates.x - 1] = '\u2228';
+					ship.setCoordinates(coordinates, x);
+				} else {
+					board[coordinates.y - 1][coordinates.x - 1] = '|';
+					ship.setCoordinates(coordinates, x);
+				}
+				coordinates['y']++;
+			}
+		} else {
+			
+		}
+	}
 }
 
 module.exports = board;
--- simple-battle-ships/test/unit/board.test.js
+++ simple-battle-ships/test/unit/board.test.js
@@ -25,9 +25,56 @@ describe('board-test', () => {
     });
   });
 
-  describe('getBoard', () => {
-    it('should retrieve the board', () => {
-      expect(newBoard.getBoard()).toEqual(emptyBoard);
-    });
-  });
+	describe('getBoard', () => {
+		it('should retrieve the board', () => {
+			expect(newBoard.getBoard()).toEqual(emptyBoard);
+		});
+	});
+
+	describe('placeShip', () => {
+		describe('given a ship with coordinates', () => {
+			it('should place the ship on the board', () => {
+				const ship_one = new Ship('ship_one');
+
+				const expectedBoard = [
+					[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+					[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+					[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+					[-1, -1, '\u2227', -1, -1, -1, -1, -1, -1, -1],
+					[-1, -1, '|', -1, -1, -1, -1, -1, -1, -1],
+					[-1, -1, '|', -1, -1, -1, -1, -1, -1, -1],
+					[-1, -1, '\u2228', -1, -1, -1, -1, -1, -1, -1],
+					[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+					[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+					[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1]
+				];
+
+				expect(newBoard.getBoard()).toEqual(emptyBoard);
+				newBoard.placeShip(ship_one, {x: 3, y: 4});
+				expect(newBoard.getBoard()).toEqual(expectedBoard);
+			});
+		});
+		describe('given a ship with coordinates and an orientation', () => {
+			it('should pace the ship on the board in the horizontal orientation', () => {
+				const ship_one = new Ship('ship_on');
+
+				const expectedBoard = [
+					[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+					[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+					[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+					[-1, -1, '\u2225', '|', '|', '\u2226', -1, -1, -1, -1],
+					[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+					[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+					[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+					[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+					[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+					[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1]
+				];
+
+				expect(newBoard.getBoard()).toEqual(emptyBoard);
+				newBoard.placeShip(ship_one, {x: 3, y: 4}, 'horizontal');
+				expect(newBoard.getBoard()).toEqual(expectedBoard);
+			});
+		});
+	});
 });
